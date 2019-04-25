9e8dd3d5aed0da398eb475833f8954839f67d8d3
thitima1509 <jillSE1509@hotmail.com>
2019-04-25 08:14:13 +0700

    report

--- js/report.js
+++ js/report.js
@@ -207,28 +207,6 @@ $(document).ready(function () {
 });
 
 //Low product Report
-// function callAPI(){
-
-
-//     $.ajax({
-
-//         type: "GET",
-//         url: "http://localhost:60443/api/IN_ProductRequis?userID=all&RequisStatus=all",
-//         dataType: 'json',
-//         headers: {
-//             'Authorization': 'basic ' + btoa(localStorage.logUsername + ':' + localStorage.logPassword)
-//         },
-
-//         success: function (data) {
-//             console.log(data);
-
-//         },
-//         error: function (jqXHR, xhr, ajaxOptions, thrownError) {
-//             console.log("Your can't View, error is '" + thrownError + "'");
-//             //window.location.href = "index.html";
-//         }
-//     });
-// }
 
 var lowProductAll = [];
 
@@ -322,28 +300,6 @@ $(document).ready(function () {
 });
 
 //Out stock Report
-// function callAPI(){
-
-
-//     $.ajax({
-
-//         type: "GET",
-//         url: "http://localhost:60443/api/IN_ProductRequis?userID=all&RequisStatus=all",
-//         dataType: 'json',
-//         headers: {
-//             'Authorization': 'basic ' + btoa(localStorage.logUsername + ':' + localStorage.logPassword)
-//         },
-
-//         success: function (data) {
-//             console.log(data);
-
-//         },
-//         error: function (jqXHR, xhr, ajaxOptions, thrownError) {
-//             console.log("Your can't View, error is '" + thrownError + "'");
-//             //window.location.href = "index.html";
-//         }
-//     });
-// }
 
 var outStockProduct = [];
 
